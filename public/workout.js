db849d5d40d09ca4d143c79df0e6aaee20269e05
KJ <kj3641402@gmail.com>
2021-06-09 01:08:24 -0400

    added index of exercises

--- public/workout.js
+++ public/workout.js
@@ -8,7 +8,7 @@ async function initWorkout() {
 
     const workoutSummary = {
       date: formatDate(lastWorkout.day),
-      totalDuration: lastWorkout.exercises.totalDuration,
+      totalDuration: lastWorkout.exercises[0].totalDuration,
       numExercises: lastWorkout.exercises.length,
       ...tallyExercises(lastWorkout.exercises)
     };
